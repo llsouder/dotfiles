e217339821cf76f50ba40b2dabfd1570a5b8f026
Lonnie Souder <lonnie@souderfamily.net>
2017-08-07 08:23:37 -0400

    Git clone the vim plugins for clojure.

---
+++ setup.bash
@@ -0,0 +1,12 @@
+mkdir ~/.vim/bundle
+cd ~/.vim/bundle
+
+#manage my parenthesis please.
+git clone https://github.com/vim-scripts/paredit.vim
+#for commands like :find and gf
+git clone git://github.com/tpope/vim-classpath.git
+#only needed for vim version 7.3.803 or less.
+git clone https://github.com/guns/vim-clojure-static.git
+#quasi repl!
+git clone git://github.com/tpope/vim-fireplace.git
+
